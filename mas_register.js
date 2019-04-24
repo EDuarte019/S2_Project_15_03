42cfa6cf7440d3e131c5a4cd1463f0342e5d0608
Evelyn Duarte <eduarte019@west-mec.org>
2019-04-23 18:25:41 -0700

    Final Commit
    Created event handlers for IDs equal to functions. Ready for review.

---
+++ mas_register.js
@@ -0,0 +1,58 @@
+"use strict";
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 13
+   Case Problem 3
+
+
+   Filename: mas_register.js
+
+   Author:Evelyn Duarte  
+   Date: 04.23.19   
+   
+   Function List
+   =============
+   
+   formTest()
+      Performs a validation test on the selection of the conference
+      session package and the conference discount number
+   
+   calcCart()
+      Calculates the cost of the registration and saves data
+      in session storage
+      
+   writeSessionValues()
+      Writes data values from session storage in to the
+      registration summary form
+
+
+*/
+
+window.addEventListener("load", function () {
+      calcCart();
+      document.getElementById("regSubmit").onclick = sessionTest;
+
+      document.getElementById("fnBox").onblur =
+            calcCart;
+      document.getElementById("lnBox").onblur = calcCart;
+
+      document.getElementById("groupBox").onblur = calcCart;
+      document.getElementById("mailBox").onblur =
+            calcCart;
+      document.getElementById("phoneBox").onblur =
+            calcCart;
+
+      document.getElementById("banquetBox").onblur =
+            calcCart;
+      document.getElementById("sessionBox").onchange = calcCart;
+      document.getElementById("mediaCB").onclick = calcCart;
+
+});
+
+function sessiontest() {
+      if (sessionBox === -1) {
+            sessionBox.setCustomValidity("Select a Session Package");
+      } else {
+            sessionBox.setCustomValidity("");
+      }
+}
